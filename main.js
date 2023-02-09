2a897b006915748b025335d063abf68ba26af72a
Aibek9320 <123246995+Aibek9320@users.noreply.github.com>
2023-02-10 00:52:33 +0600

    21/7

--- main.js
+++ main.js
@@ -1,90 +1,92 @@
-// let a = 'tests'
-// if(a = 'test'){
-//     console.log('Верно')
-// }    
-// else{
-//     console.log('Неверно')
-// }
+// task1
+let a = 'tests'
+if(a = 'test'){
+    console.log('Верно')
+}    
+else{
+    console.log('Неверно')
+}
 
-// let m = 49
-// let n
-// if (m > 50){
-//     n = 'большое'
-// }
-// else {
-//     n = 'маленькое' 
-// }
-// console.log(n)
+// task2
+let m = 49
+let n
+if (m > 50){
+    n = 'большое'
+}
+else {
+    n = 'маленькое' 
+}
+console.log(n)
 
-// -----------------------
-// let a = 5
-// if(a === 0 || a === 2){
-//     a = a + 7 
-// }
-// else{
-//     a = a / 10
-// }
-// console.log(a)
+// taks3
+let aa = 5
+if(aa === 0 || aa === 2){
+    aa = aa + 7 
+}
+else{
+    aa = aa / 10
+}
+console.log(aa)
 
-// let b = 0
-// if(b === 0 || b === 2){
-//     b = b + 7 
-// }
-// else{
-//     b = b / 10
-// }
-// console.log(b)
+let b = 0
+if(b === 0 || b === 2){
+    b = b + 7 
+}
+else{
+    b = b / 10
+}
+console.log(b)
 
-// let c = -3
-// if(c === 0 || c === 2){
-//     c = c + 7 
-// }
-// else{
-//     c = c / 10
-// }
-// console.log(c)
+let c = -3
+if(c === 0 || c === 2){
+    c = c + 7 
+}
+else{
+    c = c / 10
+}
+console.log(c)
 
-// let v = 2
-// if(v === 0 || v === 2){
-//     v = v + 7 
-// }
-// else{
-//     v = v / 10
-// }
-// console.log(v)
+let v = 2
+if(v === 0 || v === 2){
+    v = v + 7 
+}
+else{
+    v = v / 10
+}
+console.log(v)
 
-// ---------------
-// 4 task
-// let a = 1 
-//     b = 3
-// if(a <= 1 && b >= 3){
-//     c = a + b
-// }
-// else{
-//     c = a - b
-// }
-// console.log(c)
+// task4
+let ac = 1 
+    b = 3
+if(ac <= 1 && b >= 3){
+    c = ac + b
+}
+else{
+    c = ac - b
+}
+console.log(c)
 
-// let q = 0 
-//     w = 6
-// if(q <= 1 && w >= 3){
-//     e = q + w
-// }
-// else{
-//     e = q - w
-// }
-// console.log(e)
+let q = 0 
+    w = 6
+if(q <= 1 && w >= 3){
+    e = q + w
+}
+else{
+    e = q - w
+}
+console.log(e)
 
-// let r = 3 
-//     t = 5
-// if(r <= 1 && t >= 3){
-//     y = r + t
-// }
-// else{
-//     y = r - t
-// }
-// console.log(y)
+let r = 3 
+    t = 5
+if(r <= 1 && t >= 3){
+    y = r + t
+}
+else{
+    y = r - t
+}
+console.log(y)
 
+// task5
 let seasons = 'июнь'
 switch (seasons) {
     case 'декабрь':
@@ -108,3 +110,40 @@ switch (seasons) {
         console.log('осень')
         break;
 }
+
+// task6
+let time = 55
+if(time >= 0 && time <= 15){
+    console.log('время относится к четверти часа')
+}
+if(time > 15 && time <= 30){
+    console.log('время относится к половине часа')
+}
+if(time > 30 && time <= 45){
+    console.log('время относится к 3/4 часа')
+}
+if(time > 45 && time <= 60){
+    console.log('время относится к целому часу')
+}
+else{
+    console.log('неизвестный диапазон')
+    
+}
+
+// task7
+let num = 15 
+if(0 == num % 3 && 0 == num % 5){
+    console.log('MasterYoda')
+}                            
+if(0 == num % 3){
+    console.log('Master')
+}                        
+else{
+    console.log('Yoda')
+}
+
+let num = 15
+if
+
+
+                                                                                      
