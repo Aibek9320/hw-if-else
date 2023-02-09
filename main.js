6b8f4b17a05d907badd8162fe7f75b18adca2376
Aiba <saiba9320@mail.ru>
2023-02-09 23:40:38 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,110 @@
+// let a = 'tests'
+// if(a = 'test'){
+//     console.log('Верно')
+// }    
+// else{
+//     console.log('Неверно')
+// }
+
+// let m = 49
+// let n
+// if (m > 50){
+//     n = 'большое'
+// }
+// else {
+//     n = 'маленькое' 
+// }
+// console.log(n)
+
+// -----------------------
+// let a = 5
+// if(a === 0 || a === 2){
+//     a = a + 7 
+// }
+// else{
+//     a = a / 10
+// }
+// console.log(a)
+
+// let b = 0
+// if(b === 0 || b === 2){
+//     b = b + 7 
+// }
+// else{
+//     b = b / 10
+// }
+// console.log(b)
+
+// let c = -3
+// if(c === 0 || c === 2){
+//     c = c + 7 
+// }
+// else{
+//     c = c / 10
+// }
+// console.log(c)
+
+// let v = 2
+// if(v === 0 || v === 2){
+//     v = v + 7 
+// }
+// else{
+//     v = v / 10
+// }
+// console.log(v)
+
+// ---------------
+// 4 task
+// let a = 1 
+//     b = 3
+// if(a <= 1 && b >= 3){
+//     c = a + b
+// }
+// else{
+//     c = a - b
+// }
+// console.log(c)
+
+// let q = 0 
+//     w = 6
+// if(q <= 1 && w >= 3){
+//     e = q + w
+// }
+// else{
+//     e = q - w
+// }
+// console.log(e)
+
+// let r = 3 
+//     t = 5
+// if(r <= 1 && t >= 3){
+//     y = r + t
+// }
+// else{
+//     y = r - t
+// }
+// console.log(y)
+
+let seasons = 'июнь'
+switch (seasons) {
+    case 'декабрь':
+    case 'январь':
+    case 'февраль':
+        console.log('зима')
+        break;
+    case 'апрель':
+    case 'март':
+    case 'май':
+        console.log('весна')
+        break;
+    case 'июнь':
+    case 'июль':
+    case 'август':
+        console.log('лето')
+        break;
+    case 'сентябрь':
+    case 'октябрь':
+    case 'ноябрь':
+        console.log('осень')
+        break;
+}
